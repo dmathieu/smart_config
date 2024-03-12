950c58c5430a819dacc099f56883e9e0ddba455c
dmathieu <42@dmathieu.com>
2024-03-12 14:34:12 +0100

    setup formatters

---
+++ lib/smart_config/formatters.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+module SmartConfig
+  #
+  # Formatters define behaviors to format values into another type
+  # This is especially useful for environment variables, which are always strings
+  #
+  module Formatters
+    def self.find(format)
+      SmartConfig::Formatters.const_get(camelize(format))
+    end
+
+    private
+
+    def self.camelize(format)
+      format.to_s.split('_').collect(&:capitalize).join
+    end
+  end
+end
+
+require 'smart_config/formatters/string'
+require 'smart_config/formatters/integer'
