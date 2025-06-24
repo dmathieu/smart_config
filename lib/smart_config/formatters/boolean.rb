ab4423bcfdd249449a005c49a33fcb83bd85dd2b
dmathieu <42@dmathieu.com>
2025-06-24 08:53:28 +0200

    fix rubocop

--- lib/smart_config/formatters/boolean.rb
+++ lib/smart_config/formatters/boolean.rb
@@ -5,7 +5,7 @@ module SmartConfig
     #
     # Boolean formats the value into a boolean
     class Boolean
-      def self.format(value)
+      def self.format(value) # rubocop:disable Naming/PredicateMethod -- This is not a predicate method
         ['true', '1', 1, true].include?(value)
       end
     end
